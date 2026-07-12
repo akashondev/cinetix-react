0a46ca75d7ee24015c6cee54664bb28f1b05ebab
akashondev <akashvishwakarma625@gmail.com>
2026-07-12 20:46:46 +0530

    api url added

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_BACKEND_URL;
+export const API_URL = 'https://cinetix-api-rcv5.onrender.com';
 export const SOCKET_URL = (API_URL || "").replace(/\/api\/?$/, "");
